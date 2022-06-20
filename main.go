d83ce9c9591c52b81de71a07399ca5c7ebb2b10d
lambrosopos <lambrosopos@gmail.com>
2022-06-20 23:59:43 +0900

    experimenting go http server

--- main.go
+++ main.go
@@ -1,20 +1,44 @@
 package main
 
 import (
+    "fmt"
     "io"
     "log"
     "net/http"
+    "time"
 )
 
-func main() {
-    http.HandleFunc("/", Tmp)
+var mux map[string]func(http.ResponseWriter, *http.Request)
 
-    err := http.ListenAndServe(":8080", nil)
+func main() {
+    var addr string = ":8080"
+
+    server := http.Server {
+        Addr: addr,
+        Handler: &myHandler{},
+        ReadTimeout: 5 * time.Second,
+    }
+
+    fmt.Println("Server Listening on port", addr)
+
+    mux = make(map[string]func(http.ResponseWriter, *http.Request))
+    mux["/tmp"] = Tmp
+    err := server.ListenAndServe()
     if err != nil {
         log.Fatal(err)
     }
 }
 
+type myHandler struct{}
+
+func (*myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    if h, ok := mux[r.URL.String()];ok {
+        h(w, r)
+        return
+    }
+    io.WriteString(w, "URL: " + r.URL.String())
+}
+
 func Tmp(w http.ResponseWriter, r *http.Request) {
-    io.WriteString(w, "version 1")
+    io.WriteString(w, "version 3")
 }
